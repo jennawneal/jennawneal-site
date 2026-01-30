b50fef5911f3508401120d3a52b4e034086a2752
jennawneal <74001593+jennawneal@users.noreply.github.com>
2026-01-30 14:28:41 -0500

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'current', 'past', 'contact']
+const section_names = ['home', 'networks', 'childfree', 'implementation', 'contact']
 
 
 window.addEventListener('DOMContentLoaded', event => {
